6577d898279acdc10908e7cf2fa0c9f6092b19b5
Gautier Miquet <xisabla.dev@gmail.com>
2021-02-01 15:43:54 +0100

    change login activity title

--- app/src/main/java/io/github/xisabla/appdevsec_secureapp/LoginActivity.kt
+++ app/src/main/java/io/github/xisabla/appdevsec_secureapp/LoginActivity.kt
@@ -18,6 +18,8 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        title = "Accounts: login"
     }
 
     /**
