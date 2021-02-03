d4313514971fa2ff3e7c4c1858dbc0053bbe0415
Charles <charles.deleforge@outlook.com>
2021-02-03 18:32:29 +0100

    Add activity to changePin

---
+++ app/src/main/java/io/github/xisabla/appdevsec_secureapp/model/PinChangeActivity.kt
@@ -0,0 +1,11 @@
+package io.github.xisabla.appdevsec_secureapp.model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PinChangeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pin_change)
+    }
+}
